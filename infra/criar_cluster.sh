3ef6bd3e57b10cb9a687e45faa080711fb568e79
rcdandrade <rcd_andrade@hotmail.com>
2022-11-14 22:39:51 -0300

    Delete criar_cluster.sh

--- infra/criar_cluster.sh
+++
@@ -1,1 +0,0 @@
-eksctl create cluster --name=trabfinal-richard --managed --instance-types=m5.xlarge --alb-ingress-access --node-private-networking --region=us-east-1 --nodes-min=2 --nodes-max=3 --full-ecr-access --asg-access --nodegroup-name=ng-trabfinal-richard
